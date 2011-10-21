7bde15b4b01d48cbbd4f31b2b1dfce4f492e3967
Zach Schwartz <zischwartz@gmail.com>
2011-10-20 21:33:43 -0400

    commit by fab, prepare_deploy

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,15 @@
 from fabric.api import *
 
 from fabric.contrib.console import confirm
+from contextlib import contextmanager as _contextmanager
 
 env.hosts = ['ec2-user@ec2-107-22-32-186.compute-1.amazonaws.com']
 
 env.path = '/home/ec2-user/conversation/'
 env.prj_name = 'voices'
-git_repo = 'git://github.com/zischwartz/owsvoices.git'
+env.git_repo = 'git://github.com/zischwartz/owsvoices.git'
+env.activate = 'source %sbin/activate' % env.path
+
 
 def prepare_deploy():
     local("./manage.py test posts") #chmod -x manage.py ?
@@ -14,12 +17,16 @@ def prepare_deploy():
     local("git push ")
 
 def prepare_server():
-	sudo('yum install git-core nginx -y mercurial')
+	sudo('yum install git-core nginx -y mercurial python-devel')
 	sudo('easy_install pip')
 	sudo('pip install virtualenv')
 # pip install supervisor
 
-
+@_contextmanager
+def virtualenv():
+	with cd(env.path):
+		with prefix(env.activate):
+			yield
 
 
 def first_deploy():
@@ -27,10 +34,10 @@ def first_deploy():
 	with settings(warn_only=True):
 		run('mkdir %s' % env.path)
 		with cd(env.path):
-			run("git clone %s %s" % (git_repo, code_dir))
+			run("git clone %s %s" % (env.git_repo, code_dir))
 			run("virtualenv --no-site-packages . ")
-			run("source bin/activate")
-			run("pip install -r  %s/requirements.txt" % env.prj_name )
+			with virtualenv():
+				run("pip install -r  %s/requirements.txt" % env.prj_name )
 
 			# run("git clone git@github.com:zischwartz/owsvoices.git %s" % code_dir) 
 	# with cd(code_dir):
